2c9f6ec53f681d3f941b5de3a351f49917430566
Farok-Hossain <farokafs7@gmail.comm>
2023-08-12 00:31:58 +0600

    all parts completed

---
+++ app/index.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, icons, images, SIZES } from "../constants";
+import Nearbyjobs from "../components/home/nearby/Nearbyjobs";
+import Popularjobs from "../components/home/popular/Popularjobs";
+import ScreenHeaderBtn from "../components/common/header/ScreenHeaderBtn";
+import Welcome from "../components/home/welcome/Welcome";
+import fImage from "../assets/images/farok.jpg";
+
+const Home = () => {
+  const router = useRouter();
+  const [searchTerm, setSearchTerm] = useState("");
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={fImage} dimension="100%" /> //iconUrl={images.profile}
+          ),
+          headerTitle: "",
+        }}
+      />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+          <Welcome
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            handleClick={() => {
+              if (searchTerm) {
+                router.push(`/search/${searchTerm}`);
+              }
+            }}
+          />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
